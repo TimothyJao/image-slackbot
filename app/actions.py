1a7e8663092367dd06be92e131d94115f738ea6d
timothyjao <timothy.i.jao@gmail.com>
2019-08-26 14:20:17 -0700

    corrected print mistake

--- app/actions.py
+++ app/actions.py
@@ -24,9 +24,9 @@ class Actions:
 
         for single in image_details[1]:
             selector += middle + single
-        print "-----------------------------"
+        print ("-----------------------------")
         print selector
-        print "-----------------------------"
+        print ("-----------------------------")
 
         image = soup.select(selector)[0]
         picture_url = image['src']
